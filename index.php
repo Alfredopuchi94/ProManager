81b70b61bbcdab92a5d6574fd7d91acafb6ea246
Unknown <alfredopuchi94@gmail.com>
2018-01-26 05:45:12 -0800

    add file

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form</title>
+	<link rel="stylesheet" href="./src/assets/css/app.css">
+	<link rel="stylesheet" href="./src/assets/css/bootstrap.min.css">
+</head>
+<body>
+<header>
+	<nav class="navbar navbar-primary bgnav justify-content-center">
+		<span class="navbar-brand mb-0 h1">Pro-Manager</span>
+	</nav>
+</header><!-- /header -->
+	<div class="container">
+		<div class="row my-5">
+			<div class="col-md-8 offset-md-2 my-5">
+				<div class="row">
+					<div class="col-md-12">
+						<form id="form_login_usuario">
+							<div id="form_message_usuario"></div>
+							<div class="form-group">
+								<input
+									type="text"
+									class="form-control"
+									name="usuario"
+									id="id_name_usuario"
+									placeholder="Usuario">
+									<div class="invalid-feedback">
+										El usuario es invalido
+									</div><br><br>
+							</div>
+							<div class="form-group">
+								<input
+									type="password"
+									class="form-control"
+									name="pass"
+									id="id_pass_usuario"
+									placeholder="Contraseña">
+									<div class="invalid-feedback">
+										la contraseña es invalida
+										<ul>
+											<li>caracteres minimos 8</li>
+											<li>debe contener al menos 1 letra mayúscula, 1 letra minúscula y 1 número</li>
+											<li>Puede contener caracteres especiales</li>
+										</ul>
+									</div>
+							</div><br>
+								<button type="submit" class="btn btn-primary btn-block w-50 mx-auto" style="background-color: #0072C6;"><span id="usuario_sub">Usuario</span></button>
+						</form> <br>
+						<a href="#" data-toggle="modal" data-target="#usuario_recovery" class="text-left d-block mx-auto">Olvide mi contraseña</a>
+					</div>
+
+	<!-- Usuario Recovery -->
+	<div class="modal fade" id="Usuario_recovery" tabindex="-1" role="dialog" aria-labelledby="Usuario_recovery" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<h5 class="modal-title" id="Usuario_recovery">Recovery Password</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			  <span aria-hidden="true">&times;</span>
+			</button>
+		  </div>
+				<form id="recovery_form_Usuario">
+			  <div class="modal-body">
+							<div class="form-group">
+							<input
+								type="usuario"
+								class="form-control"
+								name="usuario"
+								id="usuarioRC"
+								placeholder="usuario">
+								<div class="invalid-feedback">
+							   El usuario es invalido
+							  </div>
+						  </div>
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="submit" class="btn btn-primary">Recovery</button>
+			  </div>
+				</form>
+		</div>
+	  </div>
+	</div>
+		
+
+
+	<script src="./src/assets/js/bootstrap.min.js"></script>
+	<script src="./src/assets/js/jquery-3.2.1.min.js"></script>
+	<!-- <script src="./src/app_api/modules/Usuarioe/controller.js"></script> -->
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-info bg-info navbar-expand-lg justify-content-between">
+  <a class="navbar-brand" href="/">ProManager</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav">
+			<form class="form-inline">
+				<?php if ($status) { ?>
+					<div class="input-group">
+						<input type="text" value="Hi <?php echo $_SESSION['fname'].' '.$_SESSION['lname']; ?>" class="form-control text-center" readonly>
+			      <span class="input-group-btn">
+			        <a href="./logout.php" class="btn btn-outline-danger">Log Out</a>
+			      </span>
+			    </div>
+				<?php } ?>
+			</form>
+    	</ul>
+  </div>
+</nav>
