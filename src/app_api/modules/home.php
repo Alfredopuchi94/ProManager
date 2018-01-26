cb1995920d4eeb61991d59d1eb5f6cc6e8a6e93d
Unknown <sanchezmj228@gmail.com>
2018-01-26 15:34:19 -0430

    Agregando home

---
+++ src/app_api/modules/home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sistema de Proveeduría</title>
+	<link rel="stylesheet" href="../../assets/css/app.css">
+	<link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../../assets/css/font-awesome.min.css">
+</head>
+<body>
+	<nav class="navbar navbar-primary sticky-top bgnav justify-content-center">
+		<p class="mb-0 title-5" style="color: white;">Sistema de Proveduria</p>
+	</nav>
+	<div class="container-fluid">
+		<div class="row">
+			<nav class="col-md-2 pl-4 sidebar bg-light">
+				<div class="sidebar-sticky pt-4">
+					<h5>Menu</h5>
+					<ul class="nav flex-column">
+					  <li class="nav-item">
+					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-search colIconMenu"></span> Consulta</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-arrow-down colIconMenu"></span> Entrada</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-arrow-up colIconMenu"></span> Salida</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-file-text colIconMenu"></span> Reportes</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-user-circle-o colIconMenu"></span> Usuarios</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-sign-out colIconMenu"></span> Salir</a>
+					  </li>
+					</ul>
+				</div>
+			</nav>
+			<div class="col-md-10 my-3">
+				<div class="row align-items-center" style="height: 700px;">
+					<div class="col-md-12 pt-5">
+						<span class="fa fa-bar-chart fa-5x w-100 text-center"></span>
+						<h1 class="w-100 text-center">Bienvenidos a ProManager</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
