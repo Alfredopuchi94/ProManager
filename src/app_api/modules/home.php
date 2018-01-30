94e3b7f625945ae43e183dd9999c3aab220f4098
Unknown <sanchezmj228@gmail.com>
2018-01-30 10:27:25 -0430

    modals

--- src/app_api/modules/home.php
+++ src/app_api/modules/home.php
@@ -30,7 +30,7 @@
 					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-file-text colIconMenu"></span> Reportes</a>
 					  </li>
 					  <li class="nav-item">
-					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-user-circle-o colIconMenu"></span> Usuarios</a>
+					    <a data-toggle="modal" data-target="#exampleModalCenter" id="usuariosModal" class="nav-link colorLetraMenu" href="#"><span class="fa fa-user-circle-o colIconMenu"></span> Usuarios</a>
 					  </li>
 					  <li class="nav-item">
 					    <a class="nav-link colorLetraMenu" href="#"><span class="fa fa-sign-out colIconMenu"></span> Salir</a>
@@ -48,5 +48,26 @@
 			</div>
 		</div>
 	</div>
+	<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+	  <div class="modal-dialog modal-dialog-centered" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        ...
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<script src="../../assets/js/jquery-3.2.1.min.js"></script>
+	<script src="../../assets/js/bootstrap.min.js"></script>
 </body>
 </html>
